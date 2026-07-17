025521a523d0d9a45d5cb25d40f0272fca6ca611
ANKUR BISHYER <noobgmaster@gmail.com>
2026-07-18 02:46:13 +0530

    Fix Express wildcard route from * to /*

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use("/api/game", gameRouter);
 app.use(express.static(path.join(__dirname, "../dist")));
 
 // Handle SPA fallback - all non-api requests go to index.html
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
